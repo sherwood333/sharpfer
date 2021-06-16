4e0d8b8ebc0b48698d26c811c27e420cbbc04fab
Tarik Hijstek <t.hijstek@mailbox.org>
2021-06-16 22:24:01 +0200

    mvp complete

--- Program.cs
+++ Program.cs
@@ -10,12 +10,18 @@ namespace sharpfer
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string fileName = args[0];
+            if(string.IsNullOrEmpty(fileName)){
+                Console.WriteLine("No file given.");
+                return;
+            }
 
+            var file = new FileInfo(fileName);
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://transfer.sh");
 
-            var req = new HttpRequestMessage(HttpMethod.Put, "Program.cs");
-            req.Content = new ByteArrayContent(await File.ReadAllBytesAsync("Program.cs"));
+            var req = new HttpRequestMessage(HttpMethod.Put, file.Name);
+            req.Content = new ByteArrayContent(await File.ReadAllBytesAsync(file.FullName));
 
 
             var res = await client.SendAsync(req);
