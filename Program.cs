d71f030828838f0fdad34054e132226aaab3f93e
Tarik Hijstek <t.hijstek@mailbox.org>
2021-06-16 22:16:58 +0200

    proof of concept

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
-
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace sharpfer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://transfer.sh");
+
+            var req = new HttpRequestMessage(HttpMethod.Put, "Program.cs");
+            req.Content = new ByteArrayContent(await File.ReadAllBytesAsync("Program.cs"));
+
+
+            var res = await client.SendAsync(req);
+            var resbody = await res.Content.ReadAsStringAsync();
+
+            Console.WriteLine($"They wrote back: {res.StatusCode}, {resbody}");
         }
     }
 }
